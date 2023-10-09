e5f76fc4fdc71307b94a2995ef65e11e4edd9a15
Elizabeth Kershaw <lizzykershaw99@hotmail.com>
2023-10-09 22:13:16 +1100

    math

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+const num1 = 1;
+const num2 = 62;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 100);
+const num3 = 9;
+const num4 = 5;
+const mod = num3 % num4;
+const max = Math.max(2, 4, 12, 14, 3, 20);
+
+console.log(mod, multiply, random, max);
+
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const table = [];
+
+// for (let i = 0; i < nums.length; i++) {
+//   for (let m = 1; m <= 10; m++) {
+//     const result = nums [i] * m;
+//     table.push(result);
+//   }
+// }
+// console.log(table);
+
+// for (let i = 0; i = result.length; i*2){
+//     multiplier = result[i]++
+// }
